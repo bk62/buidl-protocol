d25a7f3164b9e4ff22018e489cd6e2783fb6a0a9
bk62 <bkafle862@gmail.com>
2022-05-07 21:54:26 +0530

    Add module base abstract contract

---
+++ contracts/core/modules/ModuleBase.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity ^0.8.0;
+
+import {Errors} from "../../libraries/Errors.sol";
+import {Events} from "../../libraries/Events.sol";
+
+/**
+ * @title Base module
+ */
+
+abstract contract ModuleBase {
+    address public immutable hub;
+
+    modifier onlyHub() {
+        if (msg.sender != hub) revert Errors.NotHub();
+        _;
+    }
+
+    constructor(address hub_) {
+        if (hub_ == address(0)) revert Errors.ConstructorParamsInvalid();
+        hub = hub_;
+        // TODO event?
+    }
+}
