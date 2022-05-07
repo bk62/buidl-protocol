bb8e4abcc4aa9ae486ef5e9b390a42287bdc4ea0
bk62 <bkafle862@gmail.com>
2022-05-07 20:28:00 +0530

    Add NFT base abstract contracts

---
+++ contracts/core/base/NFTBaseUpgradeable.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+abstract contract NFTBase is ContextUpgradeable, ERC721EnumerableUpgradeable {
+    // See "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol"
+    /**
+     * @dev Burns `tokenId`. See {ERC721-_burn}.
+     *
+     * Requirements:
+     *
+     * - The caller must own `tokenId` or be an approved operator.
+     */
+    function burn(uint256 tokenId) public virtual {
+        //solhint-disable-next-line max-line-length
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721Burnable:caller-not-owner-approved"
+        );
+        _burn(tokenId);
+    }
+}
