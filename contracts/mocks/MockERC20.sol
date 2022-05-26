879acfc4dafb2e8f2bd03b6bdb46a69a7b656c2f
bk62 <bkafle862@gmail.com>
2022-05-26 23:18:16 +0530

    Add mocks from CL starter kit

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -3,6 +3,11 @@
 pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IMockERC20Mintable is IERC20 {
+    function mint(address to, uint256 amount) external;
+}
 
 contract MockERC20 is ERC20("MockERC20", "MERC20") {
     function mint(address to, uint256 amount) external {
